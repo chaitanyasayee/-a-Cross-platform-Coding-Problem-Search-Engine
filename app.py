c0e4506a1ab0125511251a7544533c20234fd097
CHAITANYA SAYEE PADMANABHA <84325486+chaitanyasayee@users.noreply.github.com>
2023-06-21 16:43:11 +0530

    Update app.py
    erroe solved

--- app.py
+++ app.py
@@ -1,142 +1,166 @@
+from flask import Flask, jsonify, render_template, request, redirect, url_for
 import math
 import re
-from flask import Flask, render_template, request, jsonify
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+import codecs
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key'
 
-
-def load_vocab(platform):
+def load_vocab(filename):
     vocab = {}
-    with open(f'Tf-Idf/{platform}_vocab.txt', 'r') as f:
+    with open(filename, "r") as f:
         vocab_terms = f.readlines()
-    with open(f'Tf-Idf/{platform}_idf-values.txt', 'r') as f:
+    with open(filename.replace("vocab", "idf-values"), "r") as f:
         idf_values = f.readlines()
 
     for (term, idf_value) in zip(vocab_terms, idf_values):
-        vocab[term.strip()] = int(idf_value.strip())
+        vocab[term.rstrip()] = int(idf_value.rstrip())
 
     return vocab
 
-def load_documents(platform):
-    documents = []
-    with open(f'Tf-Idf/{platform}_documents.txt', 'r') as f:
+def load_document(filename):
+    with open(filename, "r") as f:
         documents = f.readlines()
-    documents = [document.strip().split() for document in documents]
 
-    print('Number of documents:', len(documents))
-    print('Sample document:', documents[0])
     return documents
 
-def load_inverted_index(platform):
+def load_inverted_index(filename):
     inverted_index = {}
-    with open(f'Tf-Idf/{platform}_inverted_index.txt', 'r') as f:
+    with open(filename, "r") as f:
         inverted_index_terms = f.readlines()
 
     for row_num in range(0, len(inverted_index_terms), 2):
         term = inverted_index_terms[row_num].strip()
-        documents = inverted_index_terms[row_num + 1].strip().split()
+        documents = inverted_index_terms[row_num+1].strip().split()
         inverted_index[term] = documents
 
-    print('Size of inverted index:', len(inverted_index))
     return inverted_index
 
-def load_Q_links(platform):
-    filename = ''
-    if platform == 'leetcode':
-        filename = 'LCQindex.txt'
-    elif platform == 'codechef':
-        filename = 'CCQindex.txt'
-    elif platform == 'codeforces':
-        filename = 'CFQindex.txt'
-    else:
-        print(f"Question links not available for platform: {platform}")
-        return {}
-
-    links = {}
-    with open(filename, 'r') as f:
-        lines = f.readlines()
-
-    for line in lines:
-        document_index, link = line.strip().split()
-        links[int(document_index)] = link
+def load_link_of_qs(filename):
+    with open(filename, "r") as f:
+        links = f.readlines()
 
     return links
 
-def get_tf_dictionary(term, platform):
-    tf_values = {}
-    if term in inverted_index[platform]:
-        for document in inverted_index[platform][term]:
-            if document not in tf_values:
-                tf_values[document] = 1
+def get_tf_dict(term, inverted_index, document):
+    tf_dict = {}
+    if term in inverted_index:
+        for doc in inverted_index[term]:
+            if doc not in tf_dict:
+                tf_dict[doc] = 1
             else:
-                tf_values[document] += 1
+                tf_dict[doc] += 1
 
-    for document in tf_values:
-        tf_values[document] /= len(documents[platform][int(document)])
+    for doc in tf_dict:
+        try:
+            tf_dict[doc] /= len(document[int(doc)])
+        except (ZeroDivisionError, ValueError, IndexError) as e:
+            print(e)
+            print(doc)
 
-    return tf_values
+    return tf_dict
 
-def get_idf_value(term, platform):
-    return math.log(len(documents[platform]) / vocab_idf_values[platform][term])
+def get_idf_value(term, vocab, document):
+    return math.log((1 + len(document)) / (1 + vocab[term]))
 
-def calculate_sorted_order_of_documents(query_terms, platforms):
-    potential_documents = {}
-    for term in query_terms:
-        for platform in platforms:
-            if term in vocab_idf_values[platform]:
-                tf_values_by_document = get_tf_dictionary(term, platform)
-                idf_value = get_idf_value(term, platform)
-                print(term, tf_values_by_document, idf_value)
-                for document in tf_values_by_document:
-                    if document not in potential_documents:
-                        potential_documents[document] = tf_values_by_document[document] * idf_value
-                    else:
-                        potential_documents[document] += tf_values_by_document[document] * idf_value
+def calc_docs_sorted_order(q_terms, vocab, document, inverted_index, Qlink):
+    potential_docs = {}
+    ans = []
+    for term in q_terms:
+        if term not in vocab:
+            continue
 
-    print(potential_documents)
-    # divide by the length of the query terms
-    for document in potential_documents:
-        potential_documents[document] /= len(query_terms)
+        tf_vals_by_docs = get_tf_dict(term, inverted_index, document)
+        idf_value = get_idf_value(term, vocab, document)
 
-    potential_documents = dict(sorted(potential_documents.items(), key=lambda item: item[1], reverse=True))
+        for doc in tf_vals_by_docs:
+            if doc not in potential_docs:
+                potential_docs[doc] = tf_vals_by_docs[doc] * idf_value
+            else:
+                potential_docs[doc] += tf_vals_by_docs[doc] * idf_value
 
-    links = {}  # Create an empty dictionary to store the links
-    for platform in platforms:
-        print(f"Results for {platform}:")
-        Q_links = load_Q_links(platform)
-        for document_index in potential_documents:
-            if int(document_index) < len(documents[platform]):
-                if potential_documents[document_index] > 0.5:
-                    links[document_index] = Q_links[int(document_index)]  # Assign the link to the document index
+        for doc in potential_docs:
+            potential_docs[doc] /= len(q_terms)
 
-    return links  # Return the dictionary of links
+        potential_docs = dict(sorted(potential_docs.items(), key=lambda item: item[1], reverse=True))
 
-platforms = ['leetcode', 'codeforces', 'codechef']
-vocab_idf_values = {platform: load_vocab(platform) for platform in platforms}
-documents = {platform: [] for platform in platforms}
-inverted_index = {platform: load_inverted_index(platform) for platform in platforms}
+        if len(potential_docs) == 0:
+            print("No matching question found. Please search with more relevant terms.")
 
-for platform in platforms:
-    documents[platform] = load_documents(platform)
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'my-secret-key'
+        for doc_index in potential_docs:
+            ans.append({"Question Link": Qlink[int(doc_index) - 1][:-2]})
+    return ans
 
 class SearchForm(FlaskForm):
-    search = StringField('Enter your search term')
-    submit = SubmitField('Search')
+    search = StringField('Search')
+   # submit = SubmitField('Submit')
 
 
 
-@app.route("/", methods=['GET', 'POST'])
-def home():
+# ...
+
+
+
+# ...
+
+@app.route('/leetcode')
+def search_leetcode():
+    query = request.args.get('query')
+    vocab = load_vocab("leetcode_vocab.txt")
+    document = load_document("leetcode_documents.txt")
+    inverted_index = load_inverted_index("leetcode_inverted_index.txt")
+    Qlink = load_link_of_qs("LCQindex.txt")
+
+    if query:
+        query = re.sub(r'[^a-zA-Z0-9\s]+', '', query.encode('utf-8').decode('unicode_escape'))
+        query_terms = query.lower().split()
+        results = calc_docs_sorted_order(query_terms, vocab, document, inverted_index, Qlink)
+        return jsonify(results)
+    else:
+        return jsonify([])
+
+@app.route('/codeforces')
+def search_codeforces():
+    query = request.args.get('query')
+    vocab = load_vocab("codeforces_vocab.txt")
+    document = load_document("codeforces_documents.txt")
+    inverted_index = load_inverted_index("codeforces_inverted_index.txt")
+    Qlink = load_link_of_qs("CFQindex.txt")
+
+    if query:
+        query = re.sub(r'[^a-zA-Z0-9\s]+', '', query.encode('utf-8').decode('unicode_escape'))
+        query_terms = query.lower().split()
+        results = calc_docs_sorted_order(query_terms, vocab, document, inverted_index, Qlink)
+        return jsonify(results)
+    else:
+        return jsonify([])
+
+@app.route('/codechef')
+def search_codechef():
+    query = request.args.get('query')
+    vocab = load_vocab("codechef_vocab.txt")
+    document = load_document("codechef_documents.txt")
+    inverted_index = load_inverted_index("codechef_inverted_index.txt")
+    Qlink = load_link_of_qs("CCQindex.txt")
+
+    if query:
+        query = re.sub(r'[^a-zA-Z0-9\s]+', '', query.encode('utf-8').decode('unicode_escape'))
+        query_terms = query.lower().split()
+        results = calc_docs_sorted_order(query_terms, vocab, document, inverted_index, Qlink)
+        return jsonify(results)
+    else:
+        return jsonify([])
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
     form = SearchForm()
-    results = []
     if form.validate_on_submit():
         query = form.search.data
-        q_terms = [term.lower() for term in query.strip().split()]
-        results = calculate_sorted_order_of_documents(q_terms,platform)[:20:]
-    return render_template('index.html', form=form, results=results)
+        return redirect(url_for('search_leetcode', query=query))
+    return render_template('index.html', form=form)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
